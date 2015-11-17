4662e76cdf46db019d4b5ae9429a1ee8f74b23f1
coderyang <coderyang137@gmail.com>
2015-11-18 03:46:50 +0800

    update utils.js

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
-var Utils = {
-  addLoadEvent: function(func) {
+;(function($){
+
+  $.addLoadEvent = function(func) {
     var old = window.onload;
     if (typeof window.onload !== 'function') {
       window.onload = func;
@@ -11,5 +12,61 @@ var Utils = {
         func();
       };
     }
+  };
+
+  $.isJQueryObj = function(elem) {
+    return typeof jQuery == "function" && elem instanceof jQuery;
+  };
+
+  $.isInViewPort = function(elem) {
+    var $elem = null,
+        rect = null;
+        $w = $(window);
+
+    if (elem.nodeType == 1) {
+      $elem = $(elem);
+    }
+
+    if ($.isJQueryObj(elem)) {
+      $elem = elem;
+    }
+
+    if ($elem[0].getBoundingClientRect) {
+      rect = $elem[0].getBoundingClientRect();
+      return !((rect.bottom < 0) || (rect.top > $w.height()));
+    }
+
+    var topOfElem = $elem.offset().top,
+        bottomOfElem = $elem.offset().top + $elem.outerHeight(),
+        scrollTop = $w.scrollTop();
+
+    return (scrollTop >= topOfElem && scrollTop < bottomOfElem ) ||
+           (scrollTop < topOfElem && scrollTop + $w.height() >= bottomOfElem) ||
+           (scrollTop + $w.height() >= topOfElem && scrollTop + $w.height() < bottomOfElem);
+  };
+
+  $.isFullInViewPort = function(elem) {
+    var $elem = null,
+        rect = null;
+    if (elem.nodeType == 1) {
+      $elem = $(elem);
+    }
+
+    if ($.isJQueryObj(elem)) {
+      $elem = elem;
+    }
+
+    if ($elem[0].getBoundingClientRect) {
+      rect = $elem[0].getBoundingClientRect();
+      return rect.top >= 0 && rect.bottom <= $(window).height(); 
+    } else {
+      var topOfElem = $elem.offset().top,
+          bottomOfElem = $elem.offset().top + $elem.outerHeight(),
+          $w = $(window),
+          scrollTop = $w.scrollTop();
+      return (scrollTop <= topOfElem && scrollTop + $w.height() >= bottomOfElem);
+    }
   }
-};
+
+})(jQuery);
+
